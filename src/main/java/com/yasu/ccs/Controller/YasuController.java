8eebc95f3970472b1b0c0e5f0d05ac5f1872bd57
godokan <117326245+godokan@users.noreply.github.com>
2023-09-12 21:03:35 +0900

    첫 화면 리다이렉트

--- src/main/java/com/yasu/ccs/Controller/YasuController.java
+++ src/main/java/com/yasu/ccs/Controller/YasuController.java
@@ -3,9 +3,21 @@ package com.yasu.ccs.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class YasuController {
+
+    @RequestMapping("/")
+    public String index() {
+            return "redirect:/home";
+    }
+
+    @GetMapping("home")
+    public String home(Model model) {
+        return "index";
+    }
+
     @GetMapping("login")
     public String login(Model model) {
         return "login";
