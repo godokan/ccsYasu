4bfa31b660afe1288279235ed82a3a40f60b16cb
godokan <nonamekoo@naver.com>
2023-09-13 15:04:38 +0900

    파일명 변경

---
+++ src/main/java/com/yasu/ccs/DTO/CcsUserDto.java
@@ -0,0 +1,33 @@
+package com.yasu.ccs.DTO;
+
+import com.yasu.ccs.Domain.Entity.CcsUserEntity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class CcsUserDto {
+    @NonNull
+    private Integer studNum;
+    private String id;
+    private String pw;
+    private String name;
+
+    @Builder
+    public CcsUserDto(@NonNull Integer studNum, String id, String pw, String name) {
+        this.studNum = studNum;
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+    }
+
+    public CcsUserEntity toEntity() {
+        System.out.println("CcsUserEntity toEntity()");
+        return CcsUserEntity.builder()
+                .studNum(studNum)
+                .id(id)
+                .pw(pw)
+                .name(name)
+                .build();
+    }
+}
