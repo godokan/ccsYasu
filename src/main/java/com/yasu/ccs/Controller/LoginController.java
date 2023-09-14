8c8056ecb817ae4a88b04558d981223b11629039
진홍기 <jjins.dev@gmail.com>
2023-09-14 22:23:11 +0900

    = 프론트엔드 = 헤더 푸터 모듈화 form 전송 데이터 json으로 형식변경
    디펜던시 변경사항
    현재 thymeleaf 템플릿 엔진 (프레임워크) 사용 중
    mustache 템플릿 엔진으로 변경

--- src/main/java/com/yasu/ccs/Controller/LoginController.java
+++ src/main/java/com/yasu/ccs/Controller/LoginController.java
@@ -1,24 +1,17 @@
 package com.yasu.ccs.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping
-    public String loginGet(Model model) {
-        return "/login";
-    }
-
-    @PostMapping
     @ResponseBody
-    public String  loginPost(@RequestBody Map<String,Object> map, Model model) {
-        model.addAttribute("user_id", map);
+    @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
+    public String  loginPost(@RequestBody Map<String,Object> map) {
+        System.out.println(map);
         return "redirect:/home";
     }
 }
