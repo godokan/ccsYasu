031d294aa24dc2576078fc9e6241becbe9578640
godokan <117326245+godokan@users.noreply.github.com>
2023-10-11 03:29:23 +0900

    일단 전체 거세

--- src/main/java/com/yasu/ccs/config/SecurityConfig.java
+++ src/main/java/com/yasu/ccs/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.yasu.ccs.config;
 
+//import com.yasu.ccs.security.APIKeyAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
@@ -17,6 +24,7 @@ import org.springframework.security.web.util.matcher.IpAddressMatcher;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private static final String[] WHITE_LIST = {
             "/",
             "/home",
@@ -34,25 +42,69 @@ public class SecurityConfig {
             "/js/**"
     };
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorizeHttpRequests) -> {
-                    authorizeHttpRequests
-                            .requestMatchers(WHITE_LIST).permitAll()
-                            .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll();
-                })
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers(WHITE_LIST);
+//    }
 
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers((headers)->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable);
-        return http.build();
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**");
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//                        .requestMatchers(WHITE_LIST).permitAll()
+//                        .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll())
+//
+//                .cors(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .logout(AbstractHttpConfigurer::disable);
+//
+//        return http.build();
+//    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
+//@Configuration
+//class ApiSecurityConfig{
+//    @Value("${projectYasu.http.auth-token-header.name}")
+//    private String principalRequestHeader;
+//
+//    @Value("${projectYasu.http.auth-token}")
+//    private String principalRequestValue;
+//
+//    @Bean
+//    public SecurityFilterChain ApisecurityFilterChain(HttpSecurity http) throws Exception {
+//        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+//        filter.setAuthenticationManager(authentication -> {
+//            String principal = (String) authentication.getPrincipal();
+//            if (!principalRequestValue.equals(principal)) {
+//                throw new BadCredentialsException("The API key was not found or not the expected value.");
+//            }
+//            authentication.setAuthenticated(true);
+//            return authentication;
+//        });
+//
+//        http
+//                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//                        .requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll())
+//                .cors(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .logout(AbstractHttpConfigurer::disable)
+//                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+//                .addFilterAfter(filter, AuthorizationFilter.class);
+//
+//        return http.build();
+//    }
+//}
--- src/main/java/com/yasu/ccs/security/APIKeyAuthFilter.java
+++ src/main/java/com/yasu/ccs/security/APIKeyAuthFilter.java
@@ -1,31 +1,31 @@
-package com.yasu.ccs.security;
-
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.stereotype.Component;
-
-@Component
-@NoArgsConstructor
-public class APIKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
-    private String principalRequestHeader;
-
-    @Autowired
-    public APIKeyAuthFilter(ApiKeyAuthManager manager) {
-        this.setAuthenticationManager(manager);
-    }
-
-    public APIKeyAuthFilter(String principalRequestHeader) {
-        this.principalRequestHeader = principalRequestHeader;
-    }
-    @Override
-    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-        return request.getHeader(principalRequestHeader);
-    }
-
-    @Override
-    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-        return "N/A";
-    }
-}
+//package com.yasu.ccs.security;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.NoArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@NoArgsConstructor
+//public class APIKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+//    private String principalRequestHeader;
+//
+//    @Autowired
+//    public APIKeyAuthFilter(ApiKeyAuthManager manager) {
+//        this.setAuthenticationManager(manager);
+//    }
+//
+//    public APIKeyAuthFilter(String principalRequestHeader) {
+//        this.principalRequestHeader = principalRequestHeader;
+//    }
+//    @Override
+//    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+//        return request.getHeader(principalRequestHeader);
+//    }
+//
+//    @Override
+//    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+//        return "N/A";
+//    }
+//}
