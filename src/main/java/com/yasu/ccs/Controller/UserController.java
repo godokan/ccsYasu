d02b6937e71b8886106336308705253263967f2d
godokan <nonamekoo@naver.com>
2023-10-23 14:45:06 +0900

    mustache 뷰리졸버 수정 2트

--- src/main/java/com/yasu/ccs/Controller/UserController.java
+++ src/main/java/com/yasu/ccs/Controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Objects;
 
-@RequestMapping
+//@RequestMapping
 @Controller
 public class UserController {
     @Autowired
