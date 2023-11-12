ae7bf00200f883e749a60f44afd234fd00747a8b
godokan <117326245+godokan@users.noreply.github.com>
2023-11-13 03:20:01 +0900

    공지사항 게시글 세부 조회

--- src/main/java/com/yasu/ccs/Controller/BoardController.java
+++ src/main/java/com/yasu/ccs/Controller/BoardController.java
@@ -1,14 +1,10 @@
 package com.yasu.ccs.Controller;
 
 import com.yasu.ccs.DTO.AlertDto;
+import com.yasu.ccs.DTO.ApiListDto;
+import com.yasu.ccs.DTO.BoardDto;
 import com.yasu.ccs.DTO.CcsUserDto;
-import com.yasu.ccs.Domain.Entity.ApiListEntity;
-import com.yasu.ccs.Domain.Entity.BoardFreeEntity;
-import com.yasu.ccs.Domain.Entity.BoardNoticeEntity;
-import com.yasu.ccs.Domain.Entity.CcsUserEntity;
-import com.yasu.ccs.Domain.Repository.ApiListRepository;
-import com.yasu.ccs.Domain.Repository.BoardFreeRepository;
-import com.yasu.ccs.Domain.Repository.BoardNoticeRepository;
+import com.yasu.ccs.Service.BoardService;
 import com.yasu.ccs.SessionConst;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
@@ -27,13 +24,9 @@ public class BoardController {
     HttpSession session;
 
     @Autowired
-    private BoardFreeRepository freeRepository;
-    @Autowired
-    private BoardNoticeRepository noticeRepository;
-    @Autowired
-    private ApiListRepository apiListRepository;
+    private BoardService boardService;
 
-    AlertDto alertDto;
+    private AlertDto alertDto;
 
     // 게시판 접속 컨트롤러
 
@@ -51,7 +44,7 @@ public class BoardController {
 
         log.info("유저 : " + sessionUser.getName()+ " / " + sessionUser.getId() + "/notice 접속");
 
-        List<BoardNoticeEntity> noticeEntities = noticeRepository.findAll();
+        List<BoardDto> noticeEntities = boardService.getNoticeList();
 
         model.addAttribute("articleList", noticeEntities);
 
@@ -72,7 +65,7 @@ public class BoardController {
 
         log.info("유저 : " + sessionUser.getName()+ " / " + sessionUser.getId() + "/freeboard 접속");
 
-        List<BoardFreeEntity> freeEntities = freeRepository.findAll();
+        List<BoardDto> freeEntities = boardService.getFreeList();
 
         model.addAttribute("articleList", freeEntities);
 
@@ -93,7 +86,7 @@ public class BoardController {
 
         log.info("유저 : " + sessionUser.getName()+ " / " + sessionUser.getId() + "/apiboard 접속");
 
-        List<ApiListEntity> apiListEntities = apiListRepository.findAll();
+        List<ApiListDto> apiListEntities = boardService.getApiList();
         model.addAttribute("apiList", apiListEntities);
 
         return "api-board";
@@ -101,5 +94,34 @@ public class BoardController {
 
     // 게시판 작성 컨트롤러
 
-    // 게시판 조회 컨트롤러 : 상세보기 페이지
+    // 게시판 조회 컨트롤러 : 상세보기 페이지.
+
+    @GetMapping("/notice/{no}")
+    public String noticeDetail(@SessionAttribute(value = SessionConst.LOGIN_USER, required = false) CcsUserDto sessionUser, @PathVariable Integer no, Model model) {
+        if (sessionUser == null) {
+            alertDto = AlertDto.builder()
+                    .message("로그인이 필요한 페이지입니다.")
+                    .redirectUrl("/home")
+                    .build();
+            model.addAttribute("message", alertDto.getMessage());
+            model.addAttribute("redirectUrl", alertDto.getRedirectUrl());
+            return "message";
+        }
+        if(no == -1) {
+            alertDto = AlertDto.builder()
+                    .message("유효하지 않은 게시글입니다.")
+                    .redirectUrl("/notice")
+                    .build();
+            model.addAttribute("message", alertDto.getMessage());
+            model.addAttribute("redirectUrl", alertDto.getRedirectUrl());
+            return "message";
+        }
+
+//        BoardDto noticeDto = boardService.getDetailNotice(no);
+
+        BoardDto noticeDto = BoardDto.builder().no(no).studNum(123456789).date("2023-11-13").context("캡스톤 디자인 프로젝트 어바웃송담 화이팅").build();
+        model.addAttribute("articleList", noticeDto);
+
+        return "notice-board-show";
+    }
 }
